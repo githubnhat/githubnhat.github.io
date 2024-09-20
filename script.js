6c9115aabfa1c6390c530ac4280abdb4366e89d6
ntran82 <nhatquangminh.tran@dxc.com>
2024-09-20 12:19:43 +0700

    new

--- script.js
+++ script.js
@@ -1,79 +1,10 @@
-function shuffle(array) {
-  let currentIndex = array.length,
-    randomIndex;
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
 
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-
-  return array;
-}
-function randomString() {
-  var input = document.querySelector(".input").value;
-  if (input == " ") {
-    alert("Nhập văn bản cần xáo trộn!");
-  }
-  console.log(input);
-  var result = "";
-  var listLines = input.split(/\r\n|\n/);
-  listLines.forEach((line) => {
-    var listWordLine = new Array();
-    var listWords = line.split(/\s/);
-    listWords.forEach((element) => {
-      var listChar = new Array();
-
-      for (let index = 0; index < element.length; index++) {
-        listChar.push(element.charAt(index));
-      }
-      shuffle(listChar);
-      var temp = "";
-      var listDots = new Array();
-      listChar.forEach((c) => {
-        if (
-          c != "." &&
-          c != "!" &&
-          c != "?" &&
-          c != "," &&
-          c != ":" &&
-          c != ";" &&
-          c != ")" &&
-          c != "("
-        ) {
-          temp += c;
-        } else {
-          listDots.push(c);
-        }
-      });
-      var dotTemp ='';
-      listDots.forEach((dot) => {
-        if (dot == "(") {
-          temp = dot + temp;
-        } else if (dot == ")") {
-          temp = temp + dot;
-        } else if (dot != " ") {
-          dotTemp = dot;
-        }
-      });
-      if (dotTemp!= " ") {
-        temp += dotTemp;
-      }
-      listWordLine.push(temp);
-    });
-    let temp1 = "";
-    listWordLine.forEach((word) => {
-      temp1 += word + " ";
-    });
-    result += temp1 + "\n";
-    console.log(result);
+yesBtn.addEventListener("click", () => {
+    // Chuyển hướng sang trang HTML mới
+    window.location.href = "ye.html";
   });
-  document.querySelector(".output").value = result;
-}
